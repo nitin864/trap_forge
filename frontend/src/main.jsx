f8139395e7318d2b122ed5124b900b61554b0160
nitin864 <rajnitin793@gmail.com>
2026-04-05 10:35:08 +0530

    removed html frontend insted used react and added routers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Landing from "./Landing.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
